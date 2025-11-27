1f91ba890aed99a32888750ee992fcee4f513bea
Arnovii <arnovi.jimenez@utp.edu.co>
2025-11-27 03:06:50 -0500

    prefix added to api endpoints

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+   app.setGlobalPrefix("api/v1");
+
   //Ejecutar las validaciones realizadas en los DTO's
   app.useGlobalPipes(
     new ValidationPipe({
