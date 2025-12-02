93e7f1a47335d814d4dd91f24b1fc19c7dd1eb52
Arnovii <arnovi.jimenez@utp.edu.co>
2025-12-02 05:44:56 -0500

    enable corse

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,15 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-   app.setGlobalPrefix("api/v1");
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization, Accept',
+    credentials: true, // si usas cookies / auth con credenciales
+    maxAge: 600, // opcional
+  });
+
+  app.setGlobalPrefix("api/v1");
 
   //Ejecutar las validaciones realizadas en los DTO's
   app.useGlobalPipes(
